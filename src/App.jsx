11ec88ee39549c26b88387de8e385fb04bcecd7b
Adit0507 <adi05072002@gmail.com>
2022-12-31 20:38:31 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-5xl ">
+           JHVE
+        </div>
+  )
+}
+
+export default App
