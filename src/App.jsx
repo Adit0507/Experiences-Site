7e076f339949283e18ba737dded4a8ace8470635
Adit0507 <adi05072002@gmail.com>
2023-01-01 17:08:50 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
+import Plan from "./components/Plan";
+import Rooms from "./components/Rooms";
+import ImageSlider from "./components/ImageSlider";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <div className="text-5xl ">
-           JHVE
-        </div>
-  )
+    <div>
+      <Navbar />
+      <Hero />
+      <Offers />
+      <Plan />
+      <Rooms />
+      <ImageSlider />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
